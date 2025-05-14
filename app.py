36b198a6360bdec64999a947e3e027e19f6a6f68
Gaurav Kuwar <gauravskuwar@gmail.com>
2025-05-14 01:06:16 -0400

    init code from https://github.com/teaching-on-testbeds/gourmetgram.git

---
+++ app.py
@@ -0,0 +1,59 @@
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+import torch
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
+
+model = torch.load("food11.pth", map_location=torch.device("cpu"), weights_only=False)
+
+def preprocess_image(img):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    return transform(img).unsqueeze(0)
+
+def model_predict(img_path, model):
+    img = Image.open(img_path).convert('RGB')
+    img = preprocess_image(img)
+
+    classes = np.array(["Bread", "Dairy product", "Dessert", "Egg", "Fried food",
+	"Meat", "Noodles/Pasta", "Rice", "Seafood", "Soup",
+	"Vegetable/Fruit"])
+
+    with torch.no_grad():
+        output = model(img)
+        prob, predicted_class = torch.max(output, 1)
+    
+    return classes[predicted_class.item()], torch.sigmoid(prob).item()
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    preds = None
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+        f.save(os.path.join(app.instance_path, 'uploads', secure_filename(f.filename)))
+        preds, probs = model_predict("./instance/uploads/" + secure_filename(f.filename), model)
+        return '<button type="button" class="btn btn-info btn-sm">' + str(preds) + '</button>' 
+    return '<a href="#" class="badge badge-warning">Warning</a>'
+
+@app.route('/test', methods=['GET'])
+def test():
+    preds, probs = model_predict("./instance/uploads/test_image.jpeg", model)
+    return str(preds)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=False)
